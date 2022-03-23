281513d88ee057e41c05f364c5d5eb8f2aa2fa3a
Bryan Dessbesell <bryanhdessbesell@gmail.com>
2022-03-23 13:45:18 -0400

    Inicializando repositorio de exercicios

---
+++ Vetor01/src/vetor01/Vetor01.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetor01;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author bryan
+ */
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num[] = {3,5,1,8,4};
+        Arrays.sort(num);
+            for(int valor: num){
+                System.out.print(valor + " ");
+        
+        /*for(int c = 0; c<= n.length-1; c++){
+            System.out.println("Na posição " + c + " temos o valor " + n[c]);*/
+        }
+    }
+    
+}
